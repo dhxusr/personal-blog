b0551eec74f9c0bb022c2aa201182a2d278352c8
dhx <juniorhx24@gmail.com>
2025-04-11 18:47:27 -0500

    chaging the ip

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ import http.server
 from backend import my_handler
 
 
-with http.server.HTTPServer(("192.168.0.18", 8000), my_handler.MyHandler) as httpd:
+with http.server.HTTPServer(("127.0.0.1", 8000), my_handler.MyHandler) as httpd:
     httpd.serve_forever()
 
 
